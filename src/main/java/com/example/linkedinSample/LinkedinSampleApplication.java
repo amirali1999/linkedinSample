977f14bbc02407c7c6bab2a78780223df85ee402
amirali1999 <85882122+amirali1999@users.noreply.github.com>
2022-10-09 17:02:22 +0330

    create project

---
+++ src/main/java/com/example/linkedinSample/LinkedinSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkedinSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinSampleApplication.class, args);
+	}
+
+}
